d7dd607b1a7eded7b6492244f6a4eaf2c2b3a9b2
Code Wolf <brgerner@gmail.com>
2021-04-09 20:39:51 -0600

    what the fuck is going on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-// how do i get the value of the timLeft when the quiz is completed/ endGame function is called
-// how do i end the game once the array has been run through?
+// how do i get the value of the timeLeft when the quiz is completed/ endGame function is called
+// how do i end the game once the array has been run through? - isLast()?
+// submit button won't present highscores page
+// isLast triggers endGame every fucking time
 
 var frontPageEl = document.querySelector('.front-page');
 var navEl = document.querySelector('nav');
@@ -13,13 +15,18 @@ var a1 = document.getElementById('0');
 var a2 = document.getElementById('1');
 var a3 = document.getElementById('2');
 var a4 = document.getElementById('3');
+var submitBtnEl = document.querySelector('#submit-initials');
+var highscoresUlEl = document.getElementsByClassName('highscores');
 
 var currentQuestionIndex = 0;
+
+
 
 var score = 0;
 var timeLeft=60;
 
 var startQuiz = function() {
+    console.log("startQuiz is running");
     frontPageEl.classList.add('hidden');
     questionsEl.classList.remove('hidden');
     startTimer();
@@ -27,6 +34,7 @@ var startQuiz = function() {
 }
 
 var startTimer = function() {
+    console.log("startTimer is running");
     var timeInterval = setInterval(function() {
         if (timeLeft > 0) {
             timerEl.textContent = timeLeft;
@@ -43,7 +51,8 @@ var startTimer = function() {
 }
 
 var nextQuestion = function() {
-    var currentQuestion = questionsArray[currentQuestionIndex]
+    console.log("nextQuestion is running");
+    var currentQuestion = questionsArray[currentQuestionIndex];
     qEl.textContent = currentQuestion.q;
     for (let i = 0; i < currentQuestion.a.length; i++) {
         const currentAnswer = currentQuestion.a[i];
@@ -51,7 +60,18 @@ var nextQuestion = function() {
     }
 }
 
+var isLast = function(i) {
+    console.log("isLast is running");
+    if (questionsArray[currentQuestionIndex] = questionsArray.length) {
+        endGame();
+    }
+    else {
+        nextQuestion();
+    }
+}
+
 var checkAnswer = function(event) {
+    console.log("checkAnswer is running");
     var answerText = event.target.textContent;
     
     if (questionsArray[currentQuestionIndex].c === answerText) {
@@ -62,11 +82,11 @@ var checkAnswer = function(event) {
         alert("Nope!")
         timeLeft = timeLeft - 10;
     }
+
     currentQuestionIndex++;
 
-    nextQuestion();
-    
-    console.log('currentQuestionIndex: ' + currentQuestionIndex);
+    isLast(currentQuestionIndex);
+
     // endGame();
     // return timeLeft
 }
@@ -75,39 +95,74 @@ var questionsArray = [
     {
         q: "What is the first parameter in the setInterval method?",
         a: ["A: time in milliseconds", "B: eventListener", "C: for loop", "D: function"],
-        c: "D: function"
+        c: "D: function",
+        l: false
     },
     {
         q: "What property is used to add a class to an element without replacing other classes already attached to the element?",
         a: ["A: element.className()", "B: element.classList.add()", "C: element.classAdd", "D: element.addClass"],
-        c: "B: element.classList.add()"
+        c: "B: element.classList.add()",
+        l: false
     },
     {
         q: "Which of the following is not equivalent in functionality to a div?",
         a: ["A: main", "B: block", "C: aside", "D: header"],
-        c: "B: block"
+        c: "B: block",
+        l: false
     },
     {
         q: "How does one remove the default dots in front of li items within a ul tag? (using CSS on the ul tag)",
         a: ["A: padding: 0", "B: display: ruby", "C: list-style: none", "D: text-decoration: none"],
-        c: "C: list-style: none"
+        c: "C: list-style: none",
+        l: false
     },
     {
         q: "How does one add a data item to local storage?",
         a: ["A: localStorage.plus('*item key*', '*item value*')", "B: localStorage.add('*item key*', '*item value*')", "C: localStorage.setItem('*item key*', '*item value*')", "D: localStorage.getItem('*item key*', '*item value*')"],
-        c: "C: localStorage.setItem('*item key*', '*item value*')"
+        c: "C: localStorage.setItem('*item key*', '*item value*')",
+        l: false
     },
+    {
+        l: true
+    }
 ];
 
 var endGame = function() {
+    console.log('endGame is running');
     questionsEl.classList.add('hidden');
     navEl.classList.add('hidden');
     initialsEl.classList.remove('hidden');
-    console.log('game has ended');
-    // return timeLeft
+
+    // return timeLeft?
 }
 
+var saveScore = function () {
+    console.log('saveScore is running');
+    var initialsInput = document.querySelector("input[name='initials-input']").value;
+    if(initialsInput.value === "") {
+        alert("Initials cannot be blank!");
+        return false
+    }
+    else {
+        var savedScores = JSON.parse(localStorage.getItem("score")) || [];
+        var currentScore = initialsInput
+        // ?.push(currentHighscore);
+        localStorage.setItem("score", initialsInput + ": " + JSON.parse(score + timeLeft));
+        showScores();
+    }
+}
 
+var showScores = function() {
+    console.log("showScores is running");
+    initialsEl.classList.add('hidden');
+    highscoreEl.classList.remove('hidden');
+    for (let i = 0; i < localStorage.length; i++) {
+        localStorage.getItem(localStorage.score(i));
+
+    }
+}
+
+submitBtnEl.addEventListener("click", saveScore);
 startBtnEl.addEventListener("click", startQuiz);
 a1.addEventListener("click", checkAnswer);
 a2.addEventListener("click", checkAnswer);
